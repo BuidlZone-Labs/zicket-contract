2a4edb8aa665b6a3395131fa70cde1b2df9b502c
codebestia <codebestia@gmail.com>
2026-02-20 11:37:02 +0100

    feat: implement ticket transfer

---
+++ contracts/ticket/src/errors.rs
@@ -0,0 +1,19 @@
+use soroban_sdk::contracterror;
+
+#[contracterror]
+#[derive(Copy, Clone, Debug, Eq, PartialEq, PartialOrd, Ord)]
+#[repr(u32)]
+pub enum TicketError {
+    TicketNotFound = 1,
+    TicketAlreadyExists = 2,
+    InvalidStatusTransition = 3,
+    Unauthorized = 4,
+    InvalidInput = 5,
+    TicketNotActive = 6,
+    InvalidTicketDate = 7,
+    InvalidTicketCount = 8,
+    InvalidPrice = 9,
+    TicketNotUpdatable = 10,
+    TicketNotTransferable = 11,
+    TransferToSelf = 12,
+}
